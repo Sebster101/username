64b27866e996c3c8bb7ffbcc3343f204671840ff
Sebster101 <sebastianludlow98@gmail.com>
2018-11-07 11:51:21 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -2,7 +2,7 @@
 # zip.sh
 echo "Enter a Username using lowercase characters and numbers between 3-12 characters: "
 read ZIP
-while echo $ZIP | egrep -v "^[a-z 0-9]{3-12}$" > /dev/null 2>&1
+while echo $ZIP | egrep -v "^[a-z0-9^!@#$%^&*()~]{3-12}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username!"
 	echo "Enter a valid username: "
